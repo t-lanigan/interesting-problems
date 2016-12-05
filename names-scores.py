f09a97f4c81c39d43a2c8d805161b61aad29349c
Tyler Lanigan <tylerlanigan@gmail.com>
2016-12-05 11:21:31 -0800

    added names-scores.py and names.txt

---
+++ names-scores.py
@@ -0,0 +1,34 @@
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text 
+# file containing over five-thousand first names, begin by sorting it into 
+# alphabetical order. Then working out the alphabetical value for each name, 
+# multiply this value by its alphabetical position in the list to obtain a 
+# name score.
+
+# For example, when the list is sorted into alphabetical order, COLIN, which 
+# is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN 
+# would obtain a score of 938 * 53 = 49714.
+
+# What is the total of all the name scores in the file?
+import time
+
+start = time.clock()
+filename = 'names.txt'
+d = open(filename).read().split(",")
+d.sort()
+
+sum_list = []
+pos = 1
+for name in d:
+    score = 0 
+    for i in name: 
+        if (ord(i) - 64) >= 1:
+            score += (ord(i) - 64)
+            
+    sum_list.append(score*pos)
+    pos += 1
+
+print sum(sum_list)
+
+end = time.clock()
+
+print 'Completed in %s seconds' % (end - start)
